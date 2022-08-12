4e41971ea54c955f4079c7c9bdcd734754b11622
Grad <edouinaw@gmail.com>
2022-08-11 23:36:54 -0500

    working on it.

--- app/src/main/java/com/example/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.foodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        print("hello there")
+        /* val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        var appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoritesFragment,
+                R.id.jokesFragment
+
+
+            )
+        )
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        setupActionBarWithNavController(navController, appBarConfiguration) */
     }
 }
