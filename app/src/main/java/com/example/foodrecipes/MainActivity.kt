47420a969f3213dbb4380dec31aa6841177fd41a
Grad <edouinaw@gmail.com>
2022-08-15 22:44:18 -0500

    August 15 work.

--- app/src/main/java/com/example/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -3,34 +3,31 @@ package com.example.foodrecipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        var appBarConfiguration = AppBarConfiguration(
+        var appBarConfiguration:AppBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.recipesFragment,
                 R.id.favoritesFragment,
                 R.id.jokesFragment
-
-
             )
         )
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomNavigationView:BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
         bottomNavigationView.setupWithNavController(navController)
 
-        setupActionBarWithNavController(navController, appBarConfiguration) */
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
